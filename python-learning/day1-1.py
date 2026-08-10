98d10b534f710b400da9632f96f1fabc2bd8efed
sevena-fable <chen20070426123@gmail.com>
2026-08-10 14:45:26 +0800

    Python Day01: variables and print

---
+++ python-learning/day1-1.py
@@ -0,0 +1,11 @@
+drug_name = "阿司匹林"
+
+drug_type = "NAISDs"
+
+targets = ["COX-1","COX-2"]
+
+print(f"药物名称是{drug_name}")
+print(f"药物类型是{drug_type}")
+print("药物靶点是")
+print(targets[0])
+print(targets[1])
---
+++ python-learning/day1-2.py
@@ -0,0 +1,17 @@
+drug_name = "阿司匹林"
+
+drug_type = "NSAIDs"
+
+weight = 180.16
+
+is_approved = True
+
+targets = ["COX-1","COX-2","PTGS"]
+
+print(f"药物名称是{drug_name}\n"
+      f"药物类型是{drug_type}\n"
+      f"分子量:{weight}\n"
+      f"是否上市：{is_approved}\n"
+      f"药物靶点是{targets[0]}\n"
+      f"药物靶点是{targets[1]}\n"
+      f"药物靶点是{targets[2]}")
